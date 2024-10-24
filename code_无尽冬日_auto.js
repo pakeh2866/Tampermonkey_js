84b439e0d9511a7b5a08986ae768985a0e669064
pakeh2866 <lushan2866@gmail.com>
2024-10-24 08:46:30 +0800

    Update code_无尽冬日_auto.js

--- code_无尽冬日_auto.js
+++ code_无尽冬日_auto.js
@@ -7,6 +7,7 @@
 // @match        https://wjdr-giftcode.centurygames.cn/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=centurygames.cn
 // @grant        none
+// @lincense     MIT
 // ==/UserScript==
 
 (function() {
